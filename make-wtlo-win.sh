a0fc15e6e72c53f8e3a8fc8884fa1ffba2cda073
KorDen <korden32@users.noreply.github.com>
2023-01-18 00:10:54 +0300

    Script v0.8

--- make-wtlo-win.sh
+++ make-wtlo-win.sh
@@ -4,22 +4,22 @@ set -e
 NGINX_VER=1.22.1
 PHP_VER=php-8.2.1-nts-Win32-vs16-x64
 WEBTLO_VER=2.9.9-alpha8
-SCRIPT_VER=0.7
+SCRIPT_VER=0.8
 
 rm -rf webtlo-win
 mkdir -p webtlo-win/php
 
 # Nginx: https://nginx.org/en/download.html
-wget --quiet -O nginx.zip "https://nginx.org/download/nginx-$NGINX_VER.zip"
+wget -nv -O nginx.zip "https://nginx.org/download/nginx-$NGINX_VER.zip"
 unzip -d webtlo-win nginx.zip
 mv "webtlo-win/nginx-$NGINX_VER" "webtlo-win/nginx"
 
 # PHP: https://windows.php.net/download Non-Thread-Safe
-wget --quiet -O php.zip "https://windows.php.net/downloads/releases/$PHP_VER.zip"
+wget -nv -O php.zip "https://windows.php.net/downloads/releases/$PHP_VER.zip"
 unzip -d webtlo-win/php php.zip
 
 # Web-TLO: https://github.com/keepers-team/webtlo
-wget --quiet -O webtlo.zip "https://github.com/keepers-team/webtlo/archive/refs/tags/$WEBTLO_VER.zip"
+wget -nv -O webtlo.zip "https://github.com/keepers-team/webtlo/archive/refs/tags/$WEBTLO_VER.zip"
 unzip -d webtlo-win webtlo.zip
 mv "webtlo-win/webtlo-$WEBTLO_VER" "webtlo-win/nginx/wtlo"
 
